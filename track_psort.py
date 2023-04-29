c2111688c412e19b8d60e647ab1002acaa2692e7
Kanya <kanyam@andrew.cmu.edu>
2023-04-29 11:12:42 -0400

    Point tracker based on MVDeTr

--- track_psort.py
+++ track_psort.py
@@ -1,3 +1,4 @@
+# will work under MVDetr
 import os
 
 os.environ['OMP_NUM_THREADS'] = '1'
@@ -7,7 +8,7 @@ import time
 import shutil
 from distutils.dir_util import copy_tree
 import datetime
-
+import random
 import tqdm
 import numpy as np
 import torch
@@ -15,36 +16,28 @@ import torch.optim as optim
 import torchvision.transforms as T
 from matplotlib import pyplot as plt
 from PIL import Image
+import csv
 
 plt.axis('off')
 
 from multiview_detector.datasets import *
-from multiview_detector.loss.gaussian_mse import GaussianMSE
-from multiview_detector.models.persp_trans_detector import PerspTransDetector
-from multiview_detector.models.image_proj_variant import ImageProjVariant
-from multiview_detector.models.res_proj_variant import ResProjVariant
-from multiview_detector.models.no_joint_conv_variant import NoJointConvVariant
-from multiview_detector.utils.logger import Logger
-from multiview_detector.utils.draw_curve import draw_curve
-from multiview_detector.utils.image_utils import img_color_denormalize, add_heatmap_to_image
-from multiview_detector.utils.meters import AverageMeter
-from multiview_detector.utils.nms import nms
-from multiview_detector.trainer import PerspectiveTrainer
-from multiview_detector.evaluation.evaluate import evaluate
+from multiview_detector.models.mvdetr import MVDeTr
 
 # from SORT.sort import *
 from psort import *
 
 from mmp_tracking_helper.mmp_mapping3D_2D_script import *
+from multiview_detector.utils.decode import ctdet_decode, mvdet_decode
+from multiview_detector.utils.nms import nms
 
 L2_THS = 8
 NMS_THS = 5
-PERSON_WIDTH = 30
-PERSON_HEIGHT = 160
 BBOX_LEN = 10
+AGE = 3  # same for max age and min hits for simplicity
+HITS = 5
+END_FRAME = -1
 
-
-def mvdet_decode(scoremap):
+def sim_decode(scoremap):
     B, C, H, W = scoremap.shape
     xy = torch.nonzero(torch.ones_like(scoremap[:, 0])).view([B, H * W, 3])[:, :, [2, 1]].float()
     scores = scoremap.permute(0, 2, 3, 1).reshape(B, H * W, 1)
@@ -94,127 +87,79 @@ def project_topdown2camera(topdown_coords, cam_id, cam_intrinsic, cam_extrinsic,
 
 
 def detect_and_track(model, log_fpath, data_loader,
-                     res_fpath=None, gt_fpath=None,
-                     visualize=False,
-                     num_cam=0, camera_intrinsic=None, camera_extrinsic=None,
-                     worldgrid2worldcoord_mat=None,
-                     coord_mapper=None):
-    losses = 0
-    precision_s, recall_s = AverageMeter(), AverageMeter()
-    all_res_list = []
+                     res_dir=None, visualize=False):
     t0 = time.time()
 
-    mot_tracker = Sort(max_age=5, min_hits=5, l2_threshold=L2_THS)  # FIXME: finetune threshold value
+    mot_tracker = Sort(max_age=AGE, min_hits=HITS, l2_threshold=L2_THS)
     colours = ['white', 'lightcoral', 'red', 'gray', 'pink', 'cyan', 'mediumorchid', 'orange',
                'gold']  # used only for display
-    # colours = np.random.rand(32, 3) #used only for display
 
-    criterion = GaussianMSE().cuda()
-    denormalize = img_color_denormalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-
-    if res_fpath is not None:
-        # assert gt_fpath is not None
-        if gt_fpath:
-            raise Warning("No gt.txt provided for moda and modp evaluation.")
-
-    for batch_idx, (data, map_gt, imgs_gt, frame) in enumerate(data_loader):
-        if frame[0] == 75:
+    for batch_idx, (data, world_gt, imgs_gt, affine_mats, frame) in enumerate(data_loader):
+        if frame[0] == END_FRAME:
             exit()
+
+        B, N = imgs_gt['heatmap'].shape[:2]
+        data = data.cuda()
+        for key in imgs_gt.keys():
+            imgs_gt[key] = imgs_gt[key].view([B * N] + list(imgs_gt[key].shape)[2:])
+        # with autocast():
         with torch.no_grad():
-            map_res, imgs_res = model(data)
-
-        if res_fpath is not None:
-            # if res_fpath is not None and gt_fpath is not None:
-            map_grid_res = map_res.detach().cpu().squeeze()
-            v_s = map_grid_res[map_grid_res > args.cls_thres].unsqueeze(1)
-            grid_ij = (map_grid_res > args.cls_thres).nonzero()
-            if data_loader.dataset.indexing == 'xy':
-                grid_xy = grid_ij[:, [1, 0]]
-            else:
-                grid_xy = grid_ij
-
-            frame_id = torch.ones_like(v_s) * frame
-            world_xy = grid_xy * data_loader.dataset.grid_reduce
-            scores = v_s
-            res = torch.cat([frame_id, world_xy.float(), scores], dim=1)
-            all_res_list.append(res)
-
-        ## Evaluation Metrics
-        # loss = 0
-        # for img_res, img_gt in zip(imgs_res, imgs_gt):
-        #     loss += criterion(img_res, img_gt.to(img_res.device), data_loader.dataset.img_kernel)
-        # loss = criterion(map_res, map_gt.to(map_res.device), data_loader.dataset.map_kernel) + \
-        #         loss / len(imgs_gt) * args.alpha
-        # losses += loss.item()
-        # pred = (map_res > args.cls_thres).int().to(map_gt.device)
-        # true_positive = (pred.eq(map_gt) * pred.eq(1)).sum().item()
-        # false_positive = pred.sum().item() - true_positive
-        # false_negative = map_gt.sum().item() - true_positive
-        # precision = true_positive / (true_positive + false_positive + 1e-4)
-        # recall = true_positive / (true_positive + false_negative + 1e-4)
-        # precision_s.update(precision)
-        # recall_s.update(recall)
+            (world_heatmap, world_offset), (imgs_heatmap, imgs_offset, imgs_wh) = model(data, affine_mats)
 
         if visualize:
             fig = plt.figure()
             subplt0 = fig.add_subplot(121, title=f"output_{batch_idx}")
             subplt1 = fig.add_subplot(122, title=f"target_{batch_idx}")
-            subplt0.imshow(map_res.cpu().detach().numpy().squeeze())
-            subplt1.imshow(criterion._traget_transform(map_res, map_gt,
-                                                       data_loader.dataset.map_kernel).cpu().detach().numpy().squeeze())
+            subplt1.imshow(world_gt['heatmap'].squeeze())
+            subplt0.imshow(world_heatmap.cpu().detach().numpy().squeeze())
             subplt0.set_axis_off()
             subplt1.set_axis_off()
-            # plt.savefig('./vis/map'+ str(batch_idx)+'.jpg')
-            # plt.close(fig)
 
-        ## NMS before tracking
-        pred = map_res
-        # TODO: try if a sigmoid is needed
-        xys = mvdet_decode(torch.sigmoid(pred.detach().cpu()))
+        # post-processing
+        xys = mvdet_decode(torch.sigmoid(world_heatmap.detach().cpu()), world_offset.detach().cpu(),
+                           reduce=data_loader.dataset.world_reduce)
         positions, scores = xys[:, :, :2], xys[:, :, 2:3]
-        ids = scores.squeeze() > 0.4
+        ids = scores.squeeze() > args.cls_thres
         pos, s = positions[0, ids], scores[0, ids, 0]
-        ids, count = nms(pos, s, 12.5, top_k=800)
+        ids, count = nms(pos, s, 20, top_k=np.inf)
         count = min(count, 20)
-        ids = ids[:count]
-        grid_xy = torch.stack([ids % 212, ids // 212]).T
+        grid_xy = pos[ids[:count]] / 2.  # [::-1, :]
+        scores = s[ids[:count]]
+        # ids = torch.stack([ids[:, 1], ids[:, 0]], dim=-1)
+        # ids = torch.stack([ids % 212, ids // 212]).T
 
-        # ids, count = nms(grid_xy.float(), scores[:,0], NMS_THS, np.inf)
-        # grid_xy = grid_xy[ids[:count], :]
-        scores = scores[0, ids, 0]
+        # xys_gt = sim_decode(world_gt["heatmap"] == torch.max(world_gt["heatmap"])).detach().cpu()
+        # positions_gt, scores_gt = xys_gt[:, :, :2], xys_gt[:, :, 2:3]
+        # ids_gt = scores_gt.squeeze() == 1
+        # pos_gt = positions_gt[0, ids_gt]
 
-        ## Pre-process detection results for tracking
-        ## FIXME: arbitrary bounding box created from point with width=10px, height=10px
         detections = []
-        for i, (fid, xy, xy_world, score) in enumerate(zip(frame_id, grid_xy, world_xy, scores)):
-            # res[1:] /= dataloader.dataset.world_reduce
-            # res[1:] -= offset[i]
-            fid = int(fid.item())
+        for i, (xy, score) in enumerate(zip(grid_xy, scores)):
             x, y = xy[0].item(), xy[1].item()
-            # print(score.shape)
             detections.append(list([x, y, score.item()]))
 
         detections = np.array(detections)
 
         ## Update tracking with current detections
         track_bbs_ids = mot_tracker.update(detections)
-        for d in track_bbs_ids:
-            ## track_bbs_idx: Nx5 (bb_x, bb_y, bb2_x, bb2_y, track_id)
-            # print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1' % (frame[0], d[4], d[0], d[1], d[2] - d[0], d[3] - d[1]))
-            if visualize:
+
+        if not visualize:
+            res_file = os.path.join(res_dir, 'topdown_' + str(batch_idx).zfill(5)+ '.csv')
+            with open(res_file, 'w', newline='') as csvfile:
+                res_writer = csv.writer(csvfile, delimiter=',')
+                for trk in track_bbs_ids:
+                    res_writer.writerow([int(trk[2])]+[trk[1]*2, trk[0]*2])
+
+        if visualize:
+            for d in track_bbs_ids:
                 d = d.astype(np.int32)
                 subplt0.add_patch(patches.Rectangle((d[0] - BBOX_LEN // 2, d[1] - BBOX_LEN // 2),
                                                     BBOX_LEN, BBOX_LEN,
                                                     fill=False, lw=1, ec=colours[d[2] % len(colours)]))
-                # subplt0.add_patch(patches.Rectangle((d[0],d[1]),d[2]-d[0],d[3]-d[1],fill=False,lw=1,ec=colours[d[4]%32,:]))
 
-        if visualize:
             topdown_filename = log_fpath + f'_nms{NMS_THS}_l2{L2_THS}_' + str(batch_idx).zfill(5) + '_bev.jpg'
             fig.savefig(topdown_filename)
             plt.close(fig)
-
-    t1 = time.time()
-    t_epoch = t1 - t0
 
     return
 
@@ -231,40 +176,24 @@ def main(args):
         torch.backends.cudnn.benchmark = True
 
     # dataset
-    normalize = T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    denormalize = img_color_denormalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    train_trans = T.Compose([T.Resize([360, 640]), T.ToTensor(), normalize, ])
-    # if 'wildtrack' in args.dataset:
-    #     data_path = os.path.expanduser('../Data/Wildtrack')
-    #     base = Wildtrack(data_path)
-    # elif 'multiviewx' in args.dataset:
-    #     data_path = os.path.expanduser('../Data/MultiviewX')
-    #     base = MultiviewX(data_path)
-    # else:
-    #     raise Exception('must choose from [wildtrack, multiviewx]')
-    # train_set = frameDataset(base, train=True, transform=train_trans, grid_reduce=4)
-    # test_set = frameDataset(base, train=False, transform=train_trans, grid_reduce=4)
+    config_file = "/mnt/Data/MVDeTr/multiview_detector/datasets/configs/retail_eval.yaml"
+    test_set = MMPframeDataset(config_file, train=False, world_reduce=args.world_reduce,
+                               img_reduce=args.img_reduce, world_kernel_size=args.world_kernel_size,
+                               img_kernel_size=args.img_kernel_size, sample_freq=args.sample_freq)
 
-    config_file = "/home/kanya/MVDet/multiview_detector/datasets/configs/retail.yaml"
-    # train_set = MMPframeDataset(config_file, train=True, sample_freq=args.sample_freq)
-    # test_set = MMPframeDataset(config_file, train=False, sample_freq=args.sample_freq)
-    test_set = MMPframeDataset(config_file, train=False, sample_freq=args.sample_freq)
-    # test_set = MMPframeDataset(config_file, train=False, sample_freq=args.sample_freq, img_reduce=4)
+    def seed_worker(worker_id):
+        worker_seed = torch.initial_seed() % 2 ** 32
+        np.random.seed(worker_seed)
+        random.seed(worker_seed)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers,
+                             pin_memory=True, worker_init_fn=seed_worker)
 
-    # train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True,
-    #                                            num_workers=args.num_workers, pin_memory=True)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=args.batch_size, shuffle=False,
-                                              num_workers=args.num_workers, pin_memory=True)
-
-    model_path = '/home/kanya/MVDet/logs/MMP_frame/default/ep10/MultiviewDetector.pth'
-    # model_path = '/home/kanya/MVDet/logs/MMP_frame/default/2023-03-26_03-18-03/MultiviewDetector.pth'
-    model = PerspTransDetector(test_set, args.arch)
+    # model_path = '/home/kanya/MVDet/logs/MMP_frame/default/ep10/MultiviewDetector.pth'
+    model_path = '/mnt/Data/MVDeTr/logs/MMP/ep10_wk5/MultiviewDetector.pth'
+    model = MVDeTr(test_set, args.arch, world_feat_arch=args.world_feat,
+                   bottleneck_dim=128, outfeat_dim=0, droupout=0.0).cuda()
     model.load_state_dict(torch.load(model_path))
     model.eval()
-
-    # trainer = PerspectiveTrainer(model, criterion, logdir, denormalize, args.cls_thres, args.alpha)
-
-    coordmap = CoordMapper(test_set.calibration_path)
 
     logdir = 'SORT_outputs'
     os.makedirs(logdir, exist_ok=True)
@@ -274,36 +203,26 @@ def main(args):
     detect_and_track(model=model,
                      log_fpath=logfp,
                      data_loader=test_loader,
-                     res_fpath=os.path.join(logdir, 'test.txt'),
-                     gt_fpath=test_set.gt_fpath,
-                     visualize=True,
-                     num_cam=test_set.num_cam,
-                     camera_intrinsic=test_set.intrinsic_matrices,
-                     camera_extrinsic=test_set.extrinsic_matrices,
-                     worldgrid2worldcoord_mat=test_set.worldgrid2worldcoord_mat,
-                     coord_mapper=coordmap)
+                     res_dir=os.path.join(logdir),
+                     visualize=args.visualize)
 
 
 if __name__ == '__main__':
     # settings
-    parser = argparse.ArgumentParser(description='Multiview detector')
-    parser.add_argument('--reID', action='store_true')
-    parser.add_argument('--cls_thres', type=float, default=0.4)
+    parser = argparse.ArgumentParser(description='Point Tracker')
+    parser.add_argument('--world_reduce', type=int, default=2)
+    parser.add_argument('--world_kernel_size', type=int, default=5)
+    parser.add_argument('--img_reduce', type=int, default=4)
+    parser.add_argument('--img_kernel_size', type=int, default=10)
+    parser.add_argument('--world_feat', type=str, default='deform_trans',
+                        choices=['conv', 'trans', 'deform_conv', 'deform_trans', 'aio'])
+    parser.add_argument('--cls_thres', type=float, default=0.1)
     parser.add_argument('--alpha', type=float, default=1.0, help='ratio for per view loss')
-    parser.add_argument('--variant', type=str, default='default',
-                        choices=['default', 'img_proj', 'res_proj', 'no_joint_conv'])
     parser.add_argument('--arch', type=str, default='resnet18', choices=['vgg11', 'resnet18'])
     parser.add_argument('-d', '--dataset', type=str, default='MMP', choices=['MMP', 'wildtrack', 'multiviewx'])
     parser.add_argument('-j', '--num_workers', type=int, default=4)
     parser.add_argument('-b', '--batch_size', type=int, default=1, metavar='N',
                         help='input batch size for training (default: 1)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.1, metavar='LR', help='learning rate (default: 0.1)')
-    parser.add_argument('--weight_decay', type=float, default=5e-4)
-    parser.add_argument('--momentum', type=float, default=0.5, metavar='M', help='SGD momentum (default: 0.5)')
-    parser.add_argument('--log_interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--resume', type=str, default=None)
     parser.add_argument('--visualize', action='store_true')
     parser.add_argument('--seed', type=int, default=1, help='random seed (default: None)')
     parser.add_argument('--sample_freq', type=int, default=1, help='sample part of frames to save time')
