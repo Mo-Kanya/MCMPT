69874cd4a51f8d3eec3c1e039850c5901efc41ab
Yeojin Jung <yj1254@nyu.edu>
2023-09-20 23:57:41 -0400

    WIP: add deepsort tracking algorithm

--- main_mvdetr_deepsort.py
+++ main_mvdetr_deepsort.py
@@ -14,10 +14,21 @@ from torch.utils.data import DataLoader
 
 from multiview_detector.datasets import *
 from multiview_detector.models.mvdetr import MVDeTr
+from multiview_detector.utils.nms import nms
 from multiview_detector.utils.logger import Logger
 from multiview_detector.utils.draw_curve import draw_curve
 from multiview_detector.utils.str2bool import str2bool
 from multiview_detector.trainer import PerspectiveTrainer
+from multiview_detector.utils.decode import ctdet_decode, mvdet_decode
+
+from deep_sort import nn_matching
+from deep_sort.tracker import Tracker
+from deep_sort.utils import visualization
+from deep_sort.detection import Detection
+
+
+NMS_THS = 12
+TOP_K = 500
 
 def main(args):
     # check if in debug mode
@@ -46,11 +57,6 @@ def main(args):
         torch.backends.cudnn.benchmark = True
     
     # dataset
-    if 'wildtrack' in args.dataset:
-        base = Wildtrack(os.path.expanduser('~/Data/Wildtrack'))
-    elif 'multiviewx' in args.dataset:
-        base = MultiviewX(os.path.expanduser('~/Data/MultiviewX'))
-    
     config_file = args.configfile
     train_set = MMPframeDataset(config_file, train=True, world_reduce=args.world_reduce,
                                 img_reduce=args.img_reduce, world_kernel_size=args.world_kernel_size,
@@ -65,9 +71,11 @@ def main(args):
         np.random.seed(worker_seed)
         random.seed(worker_seed)
     
-    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers,
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=False, 
+                              num_workers=args.num_workers,
                               pin_memory=True, worker_init_fn=seed_worker)
-    test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers,
+    test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, 
+                             num_workers=args.num_workers,
                              pin_memory=True, worker_init_fn=seed_worker)
     
     logdir = args.logdir
@@ -76,43 +84,81 @@ def main(args):
     print(vars(args))
     
     # detection_model
-    detection_model_path = os.path.join(
+    detector_path = os.path.join(
         args.detection_model_path_home, 
         args.resume,
         "MultiviewDetector.pth"
     )
-    detection_model = MVDeTr(train_set, args.arch, world_feat_arch=args.world_feat,
-                   bottleneck_dim=args.bottleneck_dim, outfeat_dim=args.outfeat_dim, droupout=args.dropout).cuda()
-    detection_model.load_state_dict(torch.load(detection_model_path))
-    detection_model.eval()
-    print('Test loaded detection model...')
+    detector = MVDeTr(train_set, args.arch, world_feat_arch=args.world_feat,
+                   bottleneck_dim=args.bottleneck_dim, outfeat_dim=args.outfeat_dim, 
+                   droupout=args.dropout).cuda()
+    detector.load_state_dict(torch.load(detector_path))
+    detector.eval()
+    print('Loaded detection model...')
     
-    param_dicts = [{"params": [p for n, p in detection_model.named_parameters() if 'base' not in n and p.requires_grad], },
-                   {"params": [p for n, p in detection_model.named_parameters() if 'base' in n and p.requires_grad],
-                    "lr": args.lr * args.base_lr_ratio, }, ]
-    # optimizer = optim.SGD(param_dicts, lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
-    optimizer = optim.Adam(param_dicts, lr=args.lr, weight_decay=args.weight_decay)
-    scaler = GradScaler()
+    metric = nn_matching.NearestNeighborDistanceMetric(
+        "cosine", args.max_cosine_distance, args.nn_budget)
+    tracker = Tracker(metric)
+    print('Loaded tracking model...')
     
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr, steps_per_epoch=len(train_loader),
-                                                    epochs=args.epochs)
-    detector = PerspectiveTrainer(detection_model, logdir, args.cls_thres, args.alpha, args.use_mse, args.id_ratio)
-    # detector.test(None, test_loader, None, visualize=True)
+    for batch_idx, (data, map_gt, imgs_gt, affine_mats, frame) in enumerate(train_loader):
+        data = data.cuda()
+        with torch.no_grad():
+            (world_heatmap, world_offset), \
+            (imgs_heatmap, imgs_offset, imgs_wh) = detector(data, affine_mats)
+        
+        xys = mvdet_decode(torch.sigmoid(world_heatmap.detach().cpu()), 
+                           world_offset.detach().cpu(),
+                           reduce=train_loader.dataset.world_reduce)
+        positions, scores = xys[:, :, :2], xys[:, :, 2:3]
+        ids = scores.squeeze() > args.cls_thres
+        pos, s = positions[0, ids], scores[0, ids, 0]
+        ids, count = nms(pos, s, 12.5, top_k=np.inf)
+        count = min(count, 20)
+        ids = pos[ids[:count]]/2.  # [::-1, :]
+        
+        ## project back to camera frames
+        
+        ## aggregate features of multiple views
+        
+        ## preprocess for tracker
+        detections = []
+        for (xy, id, score) in zip(pos, ids, s):
+            detections.append(Detection(xy, score, feature))
+        
+        tracker.predict()
+        tracker.update(detections)
+        
+        # with torch.no_grad():
+            # map_res, imgs_res = detector(data, affine_mats)
+        # map_grid_res = map_res.detach().cpu().squeeze()
+        # v_s = map_grid_res[map_grid_res > args.cls_thres].unsqueeze(1)
+        # grid_ij = (map_grid_res > args.cls_thres).nonzero()
+        # if train_loader.dataset.indexing == 'xy':
+        #     grid_xy = grid_ij[:, [1, 0]]
+        # else:
+        #     grid_xy = grid_ij
+        
+        # frame_id = torch.ones_like(v_s) * frame
+        # world_xy = grid_xy * train_loader.dataset.grid_reduce
+        # scores = v_s
+        # res = torch.cat([frame_id, world_xy.float(), scores], dim=1)
+        
+        # ## NMS before tracking
+        # ids, count = nms(grid_xy.float(), scores[:,0], NMS_THS, TOP_K)
+        # # ids, count = nms(grid_xy.float(), scores[:,0], NMS_THS, np.inf)
+        # grid_xy = grid_xy[ids[:count], :]
+        # scores = scores[ids[:count]]
     
-    world_reduce = test_loader.dataset.world_reduce
-    for data, world_gt, imgs_gt, affine_mats, frame in test_loader:
-        pos, id, cnt = detector.inference(data, world_gt, imgs_gt, affine_mats, frame, world_reduce)
-        print(pos) #1x34556x2
-        print(id) #7x2
-        print(cnt) #7
-        import pdb; pdb.set_trace()
+    
+    
     
     
 def parse_args():
     parser = argparse.ArgumentParser(description='Multiview detector')
     
     # trainer
-    parser.add_argument('--cls_thres', type=float, default=0.6)
+    parser.add_argument('--cls_thres', type=float, default=0.4)
     parser.add_argument('--alpha', type=float, default=1.0, help='ratio for per view loss')
     parser.add_argument('--use_mse', type=str2bool, default=False)
     parser.add_argument('--id_ratio', type=float, default=0)
@@ -156,7 +202,15 @@ def parse_args():
     parser.add_argument('--reID', action='store_true')
     parser.add_argument('--dropcam', type=float, default=0.0)
     parser.add_argument('--logdir', type=str, default='./logs_deepsort')
-        
+    
+    # DeepSORT
+    parser.add_argument(
+        "--max_cosine_distance", help="Gating threshold for cosine distance "
+        "metric (object appearance).", type=float, default=0.2)
+    parser.add_argument(
+        "--nn_budget", help="Maximum size of the appearance descriptors "
+        "gallery. If None, no budget is enforced.", type=int, default=None)
+    
     return parser.parse_args()
 
 if __name__ == "__main__":
